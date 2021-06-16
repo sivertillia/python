c42582634976fe887081d4ec5565c191aee235e4
sivertillya <volk.sivert@gmail.com>
2021-06-16 12:05:44 +0300

    mult perfect

--- Mult/spam_2.0/app2.py
+++ Mult/spam_2.0/app2.py
@@ -1,68 +1,55 @@
 import time
 import tkinter as tk
+from tkinter import messagebox as mb
 
 import multiprocessing
 
+thr = ''
+
 root = tk.Tk()
-spam_num = False
-print('spam /: ',spam_num)
+# root.overrideredirect(1)
 textSpam = tk.Entry(master = root, width=20)
 
-def starter_gl_pr():
-    global spam_num
-    spam_num = widthSpam.get()
-    print('Spam:', spam_num)
-
-
-
-
 def play():
-    threaded_run()
+    print('Play')
+    mp_while_test()
     
 
-def spam(a, b, c, d):
-    global spam_num
-    textasda = textSpam.get()
+def while_test(spam_num, textasda, c, d):
     spam_num = int(spam_num)
-    print('spam fun: ',spam_num)
-    # time.sleep(5)
     while 0 <= spam_num:
-        print(textasda, spam_num)
-        spam_num = spam_num-1
+        print(textasda)
         time.sleep(2)
-    return
 
-def threaded_run():
-    starter_gl_pr()
-    thr = multiprocessing.Process(target=spam, args=(1, 2), kwargs={"c": 3, "d": 4})
+def mp_while_test():
+    global thr
+    thr = multiprocessing.Process(target=while_test, args=(widthSpam.get(),textSpam.get()), kwargs = {"c": 3, "d": 4})
     thr.start()
-    check_status(thr)
-    return
 
 def stop():
-    global spam_num
-    spam_num = 0
+    global thr
+    try:
+        thr.kill()
+    except:
+        print('Error')
+    print('Stop')
 
-def check_status(mp):
-    """ p is the multiprocessing.Process object """
-    if mp.is_alive(): # Then the process is still running
-        startBtn.config(state = "disabled")
-        root.after(200, lambda mp=mp: check_status(mp)) # After 200 ms, it will check the status again.
-    else:
-        startBtn.config(state = "normal")
-    return
-
-
-
-
+def on_close():
+    if mb.askokcancel("Quit", "Do you want to quit?"):
+        try:
+            thr.kill()
+        except:
+            print('Error')
+        root.destroy()
 
 if __name__ == "__main__":
     textSpam.pack()
     widthSpam = tk.Entry(master = root, width=20)
     widthSpam.pack()
-    startBtn = tk.Button(master = root, text="Start", width=14, command=threaded_run)
+    startBtn = tk.Button(master = root, text="Start", width=14, command=play)
     startBtn.pack()
     stopBtn = tk.Button(master = root, text="Stop", width=14, command=stop)
     stopBtn.pack()
-    
+
+    root.protocol("WM_DELETE_WINDOW", on_close)
     root.mainloop()
